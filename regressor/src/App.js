c6f64b675326c02eaf35bb341e482a03790eefd0
PRITHWIRAJ SAMANTA <prskid1000@gmail.com>
2020-10-17 15:50:40 +0530

    Update App.js

--- regressor/src/App.js
+++ regressor/src/App.js
@@ -3,7 +3,7 @@ import * as tf from '@tensorflow/tfjs'
 import {Container, FormControl, Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+/* eslint-disable */
 var logger = "";
 
 function createModel() {
